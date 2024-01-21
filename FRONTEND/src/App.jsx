925cff64bb96edd4add1ba9b660e61937677042b
barkhayadav12 <143165059+barkhayadav12@users.noreply.github.com>
2024-01-21 18:15:40 +0530

    Update App.jsx

--- FRONTEND/src/App.jsx
+++ FRONTEND/src/App.jsx
@@ -23,7 +23,6 @@ function App(){
         <Route path='/writenote' element={<WriteNote/>}></Route>
         <Route path='/readnote/:id' element={<ReadNote/>}></Route>
       </Routes>
-      <Footer/>
     </BrowserRouter>
   )
 }
